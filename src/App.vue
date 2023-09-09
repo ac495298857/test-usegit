375a62934d5f2a90216f0030d6283fb3e2c01565
acc <495298857@qq.com>
2023-09-09 22:17:01 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="box">
+    <h1>你好</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 400px;
+  height: 200px;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
